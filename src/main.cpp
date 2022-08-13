00f6e11098d36ad958f7330fe6adccdf83b492f5
kir <mr.mirsaid899@gmail.com>
2022-08-13 17:42:05 +1000

    add movement

--- src/main.cpp
+++ src/main.cpp
@@ -6,27 +6,29 @@
 #include "camera2D.h"
 #include "checker.h"
 
-glm::vec2 get_position_from_coord(int i, int j);
+glm::vec2 position_from_coord(int i, int j);
+glm::ivec2 desk_coord_from_screen_coord(glm::vec2 coord);
 
 const int desk_size = 8;
 int desk[desk_size][desk_size] =
     {
-        {0, 2, 0, 2, 0, 2, 0, 2},
-        {2, 0, 2, 0, 2, 0, 2, 0},
-        {0, 2, 0, 2, 0, 2, 0, 2},
-        {0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0},
+        // inversed to better access
         {1, 0, 1, 0, 1, 0, 1, 0},
         {0, 1, 0, 1, 0, 1, 0, 1},
         {1, 0, 1, 0, 1, 0, 1, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 2, 0, 2, 0, 2, 0, 2},
+        {2, 0, 2, 0, 2, 0, 2, 0},
+        {0, 2, 0, 2, 0, 2, 0, 2},
 };
 
+Window wnd;
+Camera2D camera;
 int main()
 {
-    Window wnd;
     wnd.init(800, 600, "openGL");
     Shader shader("../resource/shaders/default.vert", "../resource/shaders/default.frag");
-    Camera2D camera;
     float camera_scale = 8;
 
     float aspect = camera_scale * ((float)wnd.getHeight() / (float)wnd.getWidth());
@@ -62,21 +64,47 @@ int main()
     Model black_model(checker_mesh, black_texture, shader);
 
     Checker chekcers[2]{
+        Checker(black_model, Checker::Color::BLACK), // black one
         Checker(white_model, Checker::Color::WHITE), // white one
-        Checker(black_model, Checker::Color::BLACK)  // black one
     };
 
+    glm::ivec2 unclicked(8, 8);
+    glm::ivec2 checker_clicked(unclicked);
+    glm::ivec2 cell_clicked(unclicked);
+
+    int last_button_state = GLFW_RELEASE;
+    int current_button_state = GLFW_RELEASE;
     while (wnd.getKey(GLFW_KEY_ESCAPE) != GLFW_PRESS && wnd.shouldClose() == 0)
     {
         glViewport(0, 0, wnd.getWidth(), wnd.getHeight());
         glClearColor(clear_color[0], clear_color[1], clear_color[2], 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT);
         //========frame start========
 
-        glm::vec<2, double> cursor_pos;
-        wnd.getCursorPos(&cursor_pos.x, &cursor_pos.y);
-        if(wnd.getMouseButton(GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS)
-            std::cout << cursor_pos.x << '\t' << cursor_pos.y << std::endl;
+        last_button_state = current_button_state;
+        current_button_state = wnd.getMouseButton(GLFW_MOUSE_BUTTON_LEFT);
+
+        if (last_button_state == GLFW_PRESS && current_button_state == GLFW_RELEASE)
+        {
+            std::cout<< "clicked" << std::endl;
+            glm::dvec2 cursor_pos;
+            wnd.getCursorPos(&cursor_pos.x, &cursor_pos.y);
+            glm::ivec2 n = desk_coord_from_screen_coord(cursor_pos);
+            if (n.x < desk_size && n.x >= 0 && n.y < desk_size && n.y >= 0)
+            {
+                if (desk[n.y][n.x] != 0) // if it is checker
+                    checker_clicked = n;
+                else // if it is empty cell
+                    cell_clicked = n;
+            }
+        }
+
+        if (checker_clicked != unclicked && cell_clicked != unclicked)
+        {
+            std::swap(desk[checker_clicked.y][checker_clicked.x], desk[cell_clicked.y][cell_clicked.x]);
+            checker_clicked = unclicked;
+            cell_clicked = unclicked;
+        }
 
         shader.use();
         shader.setCamera(camera);
@@ -90,7 +118,7 @@ int main()
                 if (curren_cell_type != 0)
                 {
                     Checker &current = chekcers[desk[i][j] % 2];
-                    current.position = get_position_from_coord(i, j);
+                    current.position = position_from_coord(i, j);
                     current.draw();
                 }
             }
@@ -111,7 +139,24 @@ int main()
     return 0;
 }
 
-glm::vec2 get_position_from_coord(int i, int j)
+glm::vec2 position_from_coord(int i, int j)
 {
     return glm::vec2(Checker::default_pos + glm::vec2(j, i));
+}
+
+glm::vec2 screen_normalized(glm::vec2 coord)
+{
+    glm::vec2 window_size(wnd.getWidth(), wnd.getHeight());
+    glm::vec2 screen_normalized;
+    screen_normalized.x = (2 * coord.x - window_size.x) / window_size.x;
+    screen_normalized.y = (-2 * coord.y + window_size.y) / window_size.y;
+    return screen_normalized;
+}
+
+glm::ivec2 desk_coord_from_screen_coord(glm::vec2 coord)
+{
+    glm::vec2 normilized = screen_normalized(coord);
+    glm::mat2 proj(camera.getProjection());
+    glm::vec2 start_pos = (proj / normilized) + glm::vec2(4, 4); // left-bottom world coord;
+    return glm::floor(start_pos);
 }
